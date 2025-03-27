e15b1fbef199851e14b3a747a95a581efec02301
UrielDev <urieel.mm@gmail.com>
2025-03-27 12:29:05 -0600

    feat: se acutaliza imagen en presentation

--- src/presentation/screens/presentation/VideoPresentation.tsx
+++ src/presentation/screens/presentation/VideoPresentation.tsx
@@ -8,10 +8,10 @@ const VideoPresentation = () => {
             <div className="relative">
               <div className="absolute inset-0 h-2/3 bg-transparent"></div>
               <div className="relative mx-auto">
-                <div className="lg:max-w-6xl lg:mx-auto">
+                <div className="lg:max-w-6xl lg:mx-auto h-full w-full">
                   <img
-                    className="transform scale-110"
-                    src="https://cdn.rareblocks.xyz/collection/clarity/images/hero/2/illustration.png"
+                    className="w-full h-full"
+                    src="https://res.cloudinary.com/dz5tntwl1/image/upload/v1741927209/OmniPixel/Captura_de_pantalla_2025-03-13_223947_hwym0i.png"
                     alt="EstateAdmin Dashboard"
                   />
                 </div>
